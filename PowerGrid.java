354e69696940504463b4c04ea56f2c7b30b5a3d9
Anwen Hao <haoanwen@gmail.com>
2025-12-09 16:51:56 -0500

    Define Edge class

--- PowerGrid.java
+++ PowerGrid.java
@@ -1,3 +1,11 @@
 public class PowerGrid {
-    
+    private class Edge {
+        private int end, weight;
+        private String name;
+        private Edge(int end, int weight, String name) {
+            this.end = end;
+            this.weight = weight;
+            this.name = name;
+        }
+    }
 }
